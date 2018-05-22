90b590b1c219d5a117f68c4bb727074468425716
Cliff Lee <lijun2005@foxmail.com>
2018-05-23 00:52:28 +0800

    add a linechart from mpandroidchart to make a demo

--- app/src/main/java/com/smartgreen/monitor/iotmonitor/MainActivity.java
+++ app/src/main/java/com/smartgreen/monitor/iotmonitor/MainActivity.java
@@ -1,7 +1,15 @@
 package com.smartgreen.monitor.iotmonitor;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setDataLineChart();
+    }
+
+    private void setDataLineChart() {
+        LineChart mLineChart = findViewById(R.id.lineChart);
+        mLineChart.setDrawBorders(true);
+        List<Entry> entries = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            entries.add(new Entry(i, (float) (Math.random()) * 80));
+        }
+        LineDataSet lineDataSet = new LineDataSet(entries, "温度");
+        LineData data = new LineData(lineDataSet);
+        mLineChart.setData(data);
     }
 }
