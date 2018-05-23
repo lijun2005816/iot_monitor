e7314a56b8df27a83a694393b1254dfa32bf6277
Cliff Lee <lijun2005@foxmail.com>
2018-05-24 00:34:02 +0800

    add point to a line dynamically as a demo

--- app/src/main/java/com/smartgreen/monitor/iotmonitor/MainActivity.java
+++ app/src/main/java/com/smartgreen/monitor/iotmonitor/MainActivity.java
@@ -2,6 +2,8 @@ package com.smartgreen.monitor.iotmonitor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -12,23 +14,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private LineChart mLineChart;
+    private LineDataSet mLineDataSet;
+    private LineData mLineData;
+    private int mMaxNum = 25;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setDataLineChart();
+        //keep screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //get linechart
+        mLineChart = findViewById(R.id.lineChart);
+        initLineChart();
     }
 
-    private void setDataLineChart() {
-        LineChart mLineChart = findViewById(R.id.lineChart);
+    private void initLineChart() {
         mLineChart.setDrawBorders(true);
         List<Entry> entries = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < mMaxNum; i++) {
             entries.add(new Entry(i, (float) (Math.random()) * 80));
         }
-        LineDataSet lineDataSet = new LineDataSet(entries, "温度");
-        LineData data = new LineData(lineDataSet);
-        mLineChart.setData(data);
+        mLineDataSet = new LineDataSet(entries, "温度");
+        mLineData = new LineData(mLineDataSet);
+        mLineChart.setData(mLineData);
+    }
+
+    public void onBtnRefreshClick(View view) {
+        Entry entry = new Entry(mLineDataSet.getEntryCount(), (float) (Math.random()) * 80);
+        mLineData.addEntry(entry, 0);
+        mLineData.notifyDataChanged();
+        mLineChart.notifyDataSetChanged();
+        mLineChart.setVisibleXRangeMaximum(mMaxNum);
+        mLineChart.moveViewToX(mLineData.getEntryCount() - 5);
     }
 }
